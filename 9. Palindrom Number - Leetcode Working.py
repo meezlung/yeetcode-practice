05cf40b2ae243215c7b435bb01fb9e511ed31733
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-01-26 23:28:09 +0800

    Add 9. Palindrome Number with working recursive reverse solution

--- 9. Palindrom Number - Leetcode Working.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, x):
-        pass
-    
-solution = Solution()
-print(solution.isPalindrome(121))
-print(solution.isPalindrome(-121))
-print(solution.isPalindrome(10))
