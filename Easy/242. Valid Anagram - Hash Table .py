7697c22adf05592c7d9b6dfcd5f19f7bc71e2247
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-02-01 18:38:54 +0800

    Update Hash Table instead of Counter

--- Easy/242. Valid Anagram - Hash Table .py
+++ Easy/242. Valid Anagram - Hash Table .py
@@ -5,7 +5,19 @@ class Solution(object):
         if len(s) != len(t):
             return False
         
-        return Counter(s) == Counter(t)
+        char_count = {}
+
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1 # counts every letter in s
+
+        for char in t:
+            # if current char in t is in char_count; or
+            # if the current count of char in char_count is 0, which implies if it is non-existent in char_count
+            if char not in char_count or char_count[char] == 0:
+                return False
+            char_count[char] -= 1 # keeps on subtracting until it makes sense in the end
+
+        return True
 
 solution = Solution()
 print(solution.isAnagram("anagram", "nagaram")) # =. true
