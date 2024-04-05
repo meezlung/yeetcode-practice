58c64ccdbb5135bb3bb854ea4dce15991de35fa1
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-05 16:56:24 +0800

    Create 1614. Maximum Nesting Depth of the Parenthesis - String Optimized.py

---
+++ Easy/1614. Maximum Nesting Depth of the Parenthesis - String Optimized.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxDepth(self, s):
+        max_depth = 0
+        score = 0
+
+        for i in s:
+            if i == "(":
+                score += 1
+            elif i == ")":
+                max_depth = max(score, max_depth)
+                score -= 1
+
+        return max_depth
+            
+solution = Solution()
+print(solution.maxDepth("(1+(2*3)+((8)/4))+1"))
+print(solution.maxDepth("(1)+((2))+(((3)))"))
+print(solution.maxDepth("()"))
+print(solution.maxDepth("(())"))
+print(solution.maxDepth("(1())"))
