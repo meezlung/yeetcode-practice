a7da9a8531624c5697534bcc0b793acecf59b48e
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-06 13:23:49 +0800

    Update 271. Encode and Decode Strings.py

--- Medium/271. Encode and Decode Strings.py
+++ Medium/271. Encode and Decode Strings.py
@@ -1,35 +1,63 @@
-class Solution(object):
+# class Solution(object):
+#     def encode(self, strs: list[str]):
+#         encoded = ""
+#         for s in strs:
+#             encoded += "#" + str(len(s)) + s
+
+#         return encoded
+
+#     def decode(self, str: str):
+#         decoded = []
+#         i = 0
+
+#         while i < len(str):
+#             word = []
+#             length = 0
+#             if str[i] == "#":
+#                 length = int(str[i + 1])
+
+#                 for j in range(i + 2, i + length + 2):
+#                     word.append(str[j])
+
+#             decoded.append(''.join(word)) 
+#             i += 2 + length
+#         return decoded
+
+class Solution:
+    
     def encode(self, strs: list[str]) -> str:
-        placeholder = ["#"]
-        for str in strs:
-            placeholder.append(str)
-            placeholder.append("#")
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+
+    def decode(self, s: str) -> list[str]:
+        res = []
+        i = 0
         
-        return "".join(placeholder)
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            print('i', j)
+            print('j', j)
 
-    def decode(self, str: str) -> list[str]:
-        if str == "##":
-            return [""]
-
-        original_word = []
-        word = []
-
-        for letter in str:
-            if letter != "#":
-                word.append(letter)
-            else:
-                if word:
-                    original_word.append("".join(word))
-                    word = []
-        
-        return original_word
-
+            print(s[i:j])
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+            
+        return res 
 
 solution = Solution()
-print(solution.encode(["neet","code","love","you"]))
-print(solution.decode("#neet#code#love#you#"))
-print(solution.encode([""]))
-print(solution.decode("##"))
-print(solution.encode(["a"]))
-print(solution.decode("#a#"))
+# print(solution.encode(["neet","code","love","you"]))
+# print(solution.decode("#4neet#4code#4love#3you"))
+# print(solution.encode([""]))
+# print(solution.decode("#0"))
+# print(solution.encode(["a"]))
+# print(solution.decode("#1a"))
+print(solution.encode(["we","say",":","yes","!@#$%^&*()"]))
+print(solution.decode("2#we3#say1#:3#yes10#!@#$%^&*()"))
 
