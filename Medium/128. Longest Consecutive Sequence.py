41e64997a21b06cba79f9ded44c7f9d074d01873
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-07 22:09:02 +0800

    Create 128. Longest Consecutive Sequence.py

---
+++ Medium/128. Longest Consecutive Sequence.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        nums = set(nums)
+
+        for num in nums:
+            # check if the num smaller than the current num
+            # is in keys, then stop because what we need is to
+            # find the minimum
+
+            if num - 1 in nums: continue 
+
+            current = 1
+            while num + 1 in nums:
+                num += 1 # bruh we can manipulate the num here
+                current += 1
+            
+            count = max(current, count)
+
+        return count
+
+
+
+        
+solution = Solution()
+print(solution.longestConsecutive([100, 4, 200, -1, 0]))
+print(solution.longestConsecutive([100,4,200,1,3,2]))
+print(solution.longestConsecutive([100,200,201,202,2,3]))
