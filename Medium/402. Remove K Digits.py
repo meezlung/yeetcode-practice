b166cc711f00b11bdd2e4397801998910d4e2c1c
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-12 00:31:36 +0800

    Update 402. Remove K Digits.py

--- Medium/402. Remove K Digits.py
+++ Medium/402. Remove K Digits.py
@@ -1,5 +1,3 @@
-from itertools import combinations
-
 class Solution(object):
     def removeKdigits(self, num, k):
         """
@@ -7,43 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
+        stack = []
+
+        for c in num:
+            print('C', c)
+            while k > 0 and stack and stack[-1] > c:
+                # if new c is lesser than current c in the last
+                # stack, then pop the current sht then append the
+                # new c
+                k -= 1
+                stack.pop()
+                print('Stack', stack)
+            
+            stack.append(c)
+            print('Stack', stack)
+
+        stack = stack[:len(stack) - k]
+        return str(int(''.join(stack))) if stack else "0"
         
-        index = []
-
-        for i in range(len(num)):
-            index.append(i)
-
-        combinations_of_index = combinations(index, k)
-
-        possible_combinations = []
-
-        for combination in combinations_of_index:
-
-            temporary = []
-
-            for i in range(len(num)):
-                if i not in combination:
-                    temporary.append(num[i])
-
-            if ''.join(temporary) != '':
-                possible_combinations.append(''.join(temporary))
-
-        return str(int(min(possible_combinations))) if possible_combinations else "0"
-
-        # possible_numbers = []
-
-        # for i in range(0, len(num) - k + 1):
-        #     print(num[i:i+k])
-        #     if num[:i] + num[i+k:]:
-        #         print(num[:i] + num[i+k:])
-        #         print(int(num[:i] + num[i+k:]))
-        #         print()
-        #         possible_numbers.append(int(num[:i] + num[i+k:]))
-        # print()
-        # return str(min(possible_numbers)) if possible_numbers else "0"
-
-
-
 solution = Solution()
 print(solution.removeKdigits("1432219", 3))
 print(solution.removeKdigits("10200", 1))
