8baf3b39c4fc0fc7328966e3f236166e11cf43ab
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-17 12:28:13 +0800

    Update 69. Sqrt(x).py

--- Easy/69. Sqrt(x).py
+++ Easy/69. Sqrt(x).py
@@ -1,12 +1,35 @@
 from math import sqrt
 
 class Solution(object):
-    def mySqrt(self, x):
+    def mySqrtWithBuiltIn(self, x):
         """
         :type x: int
         :rtype: int
         """
         return int(sqrt(x))
+    
+    def mySqrt(self, x): # binary search
+        """
+        :type x: int
+        :rtype: int
+        """
+        
+        def binary_search(left, right):
+            mid = (left + right) // 2
+
+            if left > right:
+                return right
+
+            if mid * mid == x:
+                return mid
+            
+            if mid * mid > x:
+                return binary_search(left, mid-1)
+
+            if mid * mid < x:
+                return binary_search(mid+1, right)
+
+        return binary_search(0, x)
 
     
 solution = Solution()
