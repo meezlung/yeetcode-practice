3848f9c5be8c66a9f33352f0f3f7c1ace774cf08
Gabriel Mislang <gabrielmislang101@gmail.com>
2024-04-27 14:49:43 +0800

    Update 219. Contains Duplicate II.py

--- Easy/219. Contains Duplicate II.py
+++ Easy/219. Contains Duplicate II.py
@@ -6,15 +6,27 @@ class Solution(object):
         :rtype: bool
         """
         
-        for i in range(len(nums)):
-            for j in range(1, len(nums)):
+        for i in range(1, len(nums)):
+            for j in range(i):
                 if i != j and nums[i] == nums[j] and abs(i - j) <= k:
                     return True
             
         return False
     
     def containsNearbyDuplicate(self, nums, k):
-    
+        seen = set()
+
+        for i, num in enumerate(nums):
+            print(i, num)
+            if i > k:
+                print(nums[i - k - 1])
+                seen.remove(nums[i - k - 1]) # makes it faster removes the index that has been kth times, and - 1 for indexing
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
+
 solution = Solution()
 print(solution.containsNearbyDuplicate([1,2,3,1], 3))
 print(solution.containsNearbyDuplicate([1,0,1,1], 1))
